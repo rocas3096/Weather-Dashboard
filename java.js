86d5bcbd1f07d051c6c01410b748df159fb4f105
rocas3096 <ozavala3096@gmail.com>
2023-04-09 18:59:48 -0500

    Added code to save searches and then recall them from local storage

--- java.js
+++ java.js
@@ -1,6 +1,9 @@
 //call API for weather using custom API key
 let weather = {
     apiKey: "d1d6eafde5dc8b5c039167dcfa22c05c",
+    // empty search history array to be populated later
+    searchHistory: [],
+    // function for fetching weather for today and 5 day forcast using input of a city
     fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/forecast?q=" 
@@ -25,6 +28,9 @@ let weather = {
         const date = new Date (dt*1000);
         const day = date.getDate();
         const month =date.getMonth() + 1;
+        // saving and displaying information related to city searched
+        this.saveSearchHistory(name);
+        this.displaySearchHistory();
         //testing that todays data works
         console.log(name, dt, icon, description, temp, humidity, speed)
         //displays todays weather data to page
@@ -37,79 +43,107 @@ let weather = {
         document.querySelector(".temp").innerText = roundedTemp + " °F";
         document.querySelector(".humidity").innerText = "Humiditiy: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "MPH";
+        //removes the class loading which was used to hide the display for todays weather until the default search was completed as to not display default text data
         document.querySelector(".card").classList.remove("loading");
         //loop over the next five days of data
         for (let i=1; i <= 5; i++) {
             const dayData = data.list.find(
                 (item) =>
-                new Date(item.dt_txt).getHours() === 18 &&
+                new Date(item.dt_txt).getHours() === 21 &&
                 new Date(item.dt_txt).getDate() === new Date().getDate() + i
-            ); //Data for 3pm on the following 5 days
+            ); //Data for the next 5 days
             const { dt_txt } =dayData;
             const { icon } = dayData.weather[0]
             const { temp } = dayData.main;
             const { speed } = dayData.wind;
             const { humidity } = dayData.main;
             const roundedTemp = Math.round(temp);
+            const roundedWind = Math.round(speed);
             
             //create new cards for each of the days and append them to the data we have
             const fiveDayForcast = document.querySelector("#five-day-forcast");
             const box = document.createElement("div");
-            box.classList.add("col-12", "col-md-1.5", "col-lg-2");
+            box.classList.add("col-12", "col-md-2", "col-lg-2");
+            box.setAttribute("style", "margin: auto;");
             const box1 = document.createElement("div");
             box1.classList.add("card");
             const box2 = document.createElement("div");
-            box2.classList.add("row");
+            box2.classList.add("col-12", "col-md-12");
             const box3 = document.createElement("div");
-            box3.classList.add("col-12", "col-md-12");
-            const box4 = document.createElement("div");
-            box4.classList.add("card-body");
+            box3.classList.add("card-body");
             const date = new Date(dt_txt);
             const day = date.getDate();
             const month = date.getMonth() + 1;
-            box4.innerHTML= `
+            box3.innerHTML= `
             <h5 class = "card-title">${month}/${day}</h5>
             <h4 class= "card-text">${roundedTemp} °F</h4>
             <img src="https://openweathermap.org/img/wn/${icon}.png" alt="image of cloud by default; image of current weather"/>
             <p class="card-text">Humidity: ${humidity} %</p>
-            <p class="card-text">Wind: ${speed} MPH</p>
+            <p class="card-text">Wind: ${roundedWind} MPH</p>
             `;
 
-            box3.appendChild(box4);
             box2.appendChild(box3);
             box1.appendChild(box2);
             box.appendChild(box1);
             fiveDayForcast.appendChild(box);
         }
     },
-    //pulls user search infomation to be used for search
+    //pulls user search infomation to be used for search and compares if the last search matched this one, if so search does not execute 
     search: function () {
-        this.fetchWeather(document.querySelector(".form-control").value);
-        const fiveDayForcast = document.querySelector("#five-day-forcast");
-        fiveDayForcast.innerHTML = `
-        <div class="col-12 col-md-2 col-lg-2" style="opacity: 0;">
-        </div>
-        `;
-    }
+        const query = document.querySelector(".form-control").value;
+        if (query !== this.lastQuery) {
+            this.lastQuery = query;
+            this.fetchWeather(query);
+            const fiveDayForcast = document.querySelector("#five-day-forcast");
+        fiveDayForcast.innerHTML = ``;
+        }
+    },
     
+    //sets length of array for searchHistory to 7 and pushes currently searched city to the array as well as saving it to local storage
+    saveSearchHistory: function (city) {
+        if(this.searchHistory.length >=7) {
+            this.searchHistory.shift();
+        }
+        this.searchHistory.push(city);
+        localStorage.setItem("searchHistory", JSON.stringify(this.searchHistory));
+    },
+
+    //displays search history by retrieving it from local storage and then creating list elements for this data and adding them to the page. These list elements are then given event listeners so that they can be used to repeat previous searches for cities
+    displaySearchHistory: function() {
+        const searchHistoryList = document.querySelector('.search-history');
+        searchHistoryList.innerHTML = ``;
+        const storedSearchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+        this.searchHistory = storedSearchHistory;
+        this.searchHistory.forEach(city => {
+            const li = document.createElement('li');
+            li.textContent = city;
+            li.addEventListener("click" , () => {
+                this.fetchWeather(city);
+                const fiveDayForcast = document.querySelector("#five-day-forcast");
+                fiveDayForcast.innerHTML = ``;
+            });
+            searchHistoryList.appendChild(li);
+        });
+    },
 };
 
+
 //adds event listener for when submit button is clicked
-document.querySelector('button[type="submit"]').addEventListener("click", function() {
-    weather.search();
+document.querySelector('button[type="submit"]').addEventListener("click", function(event) {
+    event.preventDefault();
+    weather.search(document.querySelector(".form-control").value);
 });
 
 //adds event listener for when enter button is hit in search bar
 document.querySelector(".form-control").addEventListener("keyup", function(event){
     if (event.key == "Enter") {
-        weather.search();
+        event.preventDefault();
+        weather.search(document.querySelector(".form-control").value);
     }
 })
 
-//makes refresh button redo the search for the current city
-document.querySelector('.btn-success').addEventListener("click", function() {
-    weather.search();
-});
+//displays search history array automatically
+weather.displaySearchHistory();
 
-//loads up weather for chicago by default
+//loads up weather for chicago by default so page is not empty 
 weather.fetchWeather("Chicago");
